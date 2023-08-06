968cee9235f2f7c86f67aed2243599686378702e
vzdohin <peenky88@yandex.ru>
2023-08-06 18:30:41 +0500

    ПР15, изм порт

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const {
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
-const { PORT = 4001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
